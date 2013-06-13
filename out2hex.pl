55baba06fa15c6f28523b50965b14309d16cf662
Anton D. Kachalov <mouse@yandex-team.ru>
2013-06-13 11:59:00 +0400

    [out2jex.pl] Use -memwidth/-romwidth 16 option Signed-off-by: Anton D. Kachalov <mouse@yandex-team.ru>

--- out2hex.pl
+++ out2hex.pl
@@ -11,49 +11,41 @@ if (defined($ENV{CCS_UTILS_DIR})) {
 $print_addr = defined($ENV{PRINT_ADDR});
 
 sub out2hex {
-    my ($f1, $f2) = @_;
-    my ($l1, $l2);
-    my (@a1, @a2);
-    my $i;
-    my $n = 1;
+    my ($f) = @_;
+    my $l;
+    my @a;
+    my ($i, $k);
+    my $csum;
 
-    open F1, "<$f1";
-    open F2, "<$f2";
+    open F, "<$f";
 
-    <F1>; <F2>;
+    <F>; # skip start \x02 char
 
-    while ($l1 = <F1>) {
-        $l2 = <F2>;
-        chomp $l1;
-        chomp $l2;
-        if ($l1 =~ /^\$A([0-9a-f]+)/) {
-            chop $l1;
-            $n = 1;
+    while ($l = <F>) {
+        chomp $l;
+        if ($l =~ /^\$A([0-9a-f]+)/) {
+            chop $l;
             $addr = hex($1);
             printf OUT "\$A%06s\n", $1;
-        } elsif ("$l1$l2" =~ /^[0-9A-F]{2} /) {
-            @a1 = split / /, lc($l1);
-            @a2 = split / /, lc($l2);
+        } elsif ("$l" =~ /^[0-9A-F]{2} /) {
+            @a = split / /, lc($l);
+
             printf OUT "\$A%06x\n", $addr if ($addr == 0);
-            for ($i = 0; $i <= $#a1; $i++, $n++, $wc++) {
+            for ($i = 0; $i <= $#a; $i+=2, $wc++) {
                 printf OUT "%04x ", $addr if ($print_addr);
-                printf OUT "%s%s\n", $a1[$i], $a2[$i];
+                printf OUT "%s%s\n", $a[$i], $a[$i+1];
                 $addr++;
             }
         }
     }
-    close F2;
-    close F1;
+    close F;
 }
 
 my ($entry) = grep(/Entry Point:/, `$CCS_TOOLS/ofd2000 --obj_display=none,header $ARGV[0]`);
 chomp $entry;
 $entry =~ s/\s*Entry Point:\s+0x[0]{2}?([0-9a-f]+)\s*$/$1/;
 
-#
-# TODO: use `-memwidth 16 -romwidth 16' to create non-splitted HEX files
-#
-system("$CCS_TOOLS/hex2000 -a ".$ARGV[0]);
+system("$CCS_TOOLS/hex2000 -a -memwidth 16 -romwidth 16 ".$ARGV[0]);
 
 my $hexfile = basename($ARGV[0]);
 $hexfile =~ s/\.([a-z]+)$//;
@@ -73,10 +65,9 @@ $wc = 0;
 $addr = 0;
 
 open OUT, ">$hexfile.hex";
-for ($i = 0; $i <= $#ascii; $i+= 2) {
-    out2hex $ascii[$i+1], $ascii[$i];
+for ($i = 0; $i <= $#ascii; $i++) {
+    out2hex $ascii[$i];
     unlink $ascii[$i];
-    unlink $ascii[$i+1];
 }
 printf OUT "\$E$entry\n";
 close OUT;
